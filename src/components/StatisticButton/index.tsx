b13a52ce46636ea7234e8a4a0d1bc31d93b672ec
Sergio González Sánchez <soysergiogonzalez@outlook.es>
2024-04-12 07:59:32 -0500

    fix: user route was corrected

---
+++ src/components/StatisticButton/index.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const StatisticsButton = () => {
+  return (
+    <button className="border border-orange-500 px-8 py-4 rounded-full text-orange-500">
+      Statistics
+    </button>
+  );
+};
+
+export default StatisticsButton;
